90eb44be39cd73d8ac20343057bed7feb9f5ce2f
Supriya upadhayay <upadhayaysupriya478@email.com>
2026-09-21 21:26:38 +0545

    refactor application routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,13 @@ import "dotenv/config";
 import express from "express";
 import pool from "./db/db.js";
 import applicationRoutes from "./routes/applicationRoutes.js";
+import { logger } from "./middleware/logger.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
+
 app.use(express.json());
+app.use(logger);
 app.use("/api/applications", applicationRoutes);
 
 app.get("/", (req, res) => {
@@ -15,52 +19,7 @@ app.get("/api/health", (req, res) => {
     res.send("DevTrack API is healthy");
 });
 
-app.post("/api/applications", async (req, res) => {
-  try {
-    const { company, position, status, userId } = req.body;
-
-    if (!company || !position || !status || !userId) {
-  return res.status(400).json({
-    message: "Missing required fields"
-  });
-}
-
-    const result = await pool.query(
-      `INSERT INTO applications
-       (company, position, status, user_id)
-       VALUES ($1, $2, $3, $4)
-       RETURNING *`,
-      [company, position, status, userId]
-    );
-
-    res.status(201).json({
-      message: "Application created",
-      data: result.rows[0]
-    });
-  } catch (error) {
-  console.error(error);
-  res.status(500).json({
-    message: "Failed to create application"
-  });
-}
-});
-
-app.get("/api/applications", async (req, res) => {
-  try {
-    const result = await pool.query(
-      "SELECT * FROM applications ORDER BY created_at DESC"
-    );
-
-    res.status(200).json({
-      data: result.rows
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Failed to fetch applications"
-    });
-  }
-});
+app.use(errorHandler);
 
 pool.query("SELECT NOW()")
   .then((result) => {
